f953e8d2150411bb70360ed067051a59ffad6ada
Diana Murtazina <murka8831@yandex.ru>
2022-12-01 22:32:39 +0400

    added a controller for rooms, components for the main page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import Login from "./components/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Rooms from "./components/Rooms";
 import styled from "styled-components";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
 
 const AppContainer = styled.div`
     width: 100%;
@@ -13,18 +15,36 @@ const AppContainer = styled.div`
     align-items: center;
 `;
 
+const WelcomeText = styled.h1`
+    margin: 0;
+    color: #1976d2;
+`;
+
+const MainContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
 function App() {
     return (
         <AppContainer>
-            <BrowserRouter>
+            <WelcomeText>Welcome to Tic-Tac-toe</WelcomeText>
+            <MainContainer>Hey!</MainContainer>
+        </AppContainer>
+    );
+}
+
+{
+    /* <BrowserRouter>
                 <Header />
                 <Routes>
                     <Route path="/" element={<Login />} />
                     <Route path="/rooms" element={<Rooms />} />
                 </Routes>
-            </BrowserRouter>
-        </AppContainer>
-    );
+            </BrowserRouter> */
 }
 
 export default App;
--- socket-server/src/api/controllers/mainController.ts
+++ socket-server/src/api/controllers/mainController.ts
@@ -2,9 +2,13 @@ import { ConnectedSocket, OnConnect, SocketController, SocketIO } from "socket-c
 import { Socket, Server } from "socket.io";
 
 @SocketController()
-class MainController {
+export class MainController {
     @OnConnect()
     public onConnection(@ConnectedSocket() socket: Socket, @SocketIO() io: Server) {
         console.log("New Socket connected: ", socket.id);
+
+        // socket.on("custom_event", (data: any) => {
+        //     console.log("Data", data);
+        // });
     }
 }
