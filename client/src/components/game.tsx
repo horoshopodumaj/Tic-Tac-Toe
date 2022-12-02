47506ad7f04a5c5613c02c3f3bf12eab8c7ac16b
Diana Murtazina <murka8831@yandex.ru>
2022-12-02 22:44:39 +0400

    checking the results of the game

--- client/src/components/game.tsx
+++ client/src/components/game.tsx
@@ -84,6 +84,52 @@ export function Game() {
         isGameSaterted,
     } = useContext(gameContext);
 
+    const checkGameState = (matrix: IPlayMatrix) => {
+        for (let i = 0; i < matrix.length; i++) {
+            let row = [];
+            for (let j = 0; j < matrix[i].length; j++) {
+                row.push(matrix[i][j]);
+            }
+
+            if (row.every((value) => value && value === playerSymbol)) {
+                return [true, false];
+            } else if (row.every((value) => value && value !== playerSymbol)) {
+                return [false, true];
+            }
+        }
+
+        for (let i = 0; i < matrix.length; i++) {
+            let column = [];
+            for (let j = 0; j < matrix[i].length; j++) {
+                column.push(matrix[j][i]);
+            }
+
+            if (column.every((value) => value && value === playerSymbol)) {
+                return [true, false];
+            } else if (column.every((value) => value && value !== playerSymbol)) {
+                return [false, true];
+            }
+        }
+
+        if (matrix[1][1]) {
+            if (matrix[0][0] === matrix[1][1] && matrix[2][2] === matrix[1][1]) {
+                if (matrix[1][1] === playerSymbol) return [true, false];
+                else return [false, true];
+            }
+
+            if (matrix[2][0] === matrix[1][1] && matrix[0][2] === matrix[1][1]) {
+                if (matrix[1][1] === playerSymbol) return [true, false];
+                else return [false, true];
+            }
+        }
+
+        if (matrix.every((m) => m.every((v) => v !== null))) {
+            return [true, true];
+        }
+
+        return [false, false];
+    };
+
     const updateGameMatrix = (column: number, row: number, symbol: "x" | "o") => {
         const newMatrix = [...matrix];
 
@@ -120,6 +166,10 @@ export function Game() {
         handleGameStart();
     }, []);
 
+    useEffect(() => {
+        checkGameState(matrix);
+    }, [matrix]);
+
     return (
         <GameContainer>
             {!isGameSaterted && <h2>Wait for the opponent to start the game!</h2>}
@@ -129,6 +179,7 @@ export function Game() {
                     <RowContainer key={rowIndex}>
                         {row.map((column, columnIndex) => (
                             <Cell
+                                key={columnIndex}
                                 onClick={() =>
                                     updateGameMatrix(columnIndex, rowIndex, playerSymbol)
                                 }>
